5c05b2694c912b65ea58104d26f47138fa4acc9c
zhangze <1183460943@qq.com>
2023-10-02 11:11:31 +0800

    init

--- src/page/list/index.vue
+++ src/page/list/index.vue
@@ -1,4 +1,3 @@
-
 <template>
     <a-button @click="openDir">
         打开I18n文件夹
@@ -6,37 +5,49 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import {ref} from 'vue'
 import {message} from "ant-design-vue";
 
-async function openDir(){
+async function openDir() {
     let dir = null;
-    try{
+    try {
         dir = await showDirectoryPicker()
-    }catch (e){
+    } catch (e) {
         message.error('打开文件夹失败')
         return;
     }
     let fileList = await dir.entries();
     let returnList = []
 
-    for await (let file of fileList) {
-        await new Promise( async (resolve, reject) => {
-            const reader = new FileReader();
-            let fileObject = await file[1].getFile()
-            reader.readAsText( fileObject, "utf-8");
-            reader.onload = function (e) {
-                console.log(e.target.result)
-                returnList.push({
-                    name: file[0],
-                    content: e.target.result
-                })
-                resolve()
-            }
-        })
+    try {
+        for await (let file of fileList) {
+            await new Promise(async (resolve, reject) => {
+                const reader = new FileReader();
+                let fileObject = await file[1].getFile()
+                reader.readAsText(fileObject, "utf-8");
+                console.log(file[1])
+
+                reader.onload = function (e) {
+                    returnList.push({
+                        name: file[0],
+                        content: e.target.result,
+                        file: file[1]
+                    })
+                    resolve()
+                }
+            })
+        }
+    } catch (e) {
+        console.log(e)
     }
     console.log(returnList)
 }
+
+
+// let writable = await file[1].createWritable();
+// writable.write('123')
+// await writable.close();
+
 </script>
 
 
